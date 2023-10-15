0b4ac479b39c66bfa58ea826c93557886c3be5dc
A-huseyn0ff <tu7ej3brm@code.edu.az>
2023-10-15 16:50:51 -0700

    js code added!

---
+++ script.js
@@ -0,0 +1,68 @@
+//task1
+
+// const arr=[2,1,3,6,4]
+// let max = arr[0], min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//          max = arr[i]; 
+//     }
+//     if (arr[i] < min) {
+//          min = arr[i]; 
+//     }
+// }
+// let sum=0;
+// arr.forEach(elements =>sum+=elements)
+// if (arr>max,min) {
+//     sum=sum-max-min
+// }
+//  console.log(sum);
+
+//----------------------------------------------
+//task2
+
+// const arr =  [3,2,1,4,3,1,5,6,9,3,2,52,3,2]
+// const result=arr.filter((a)=>a===3)
+// let size = result.length
+// console.log(size);
+
+//-----------------------------------------------
+//task3
+
+// const arr=[1,5,3,2,7,2,4,3,2,6]
+// let max = arr[0]
+// let sum=0;
+// for (let i = 0; i < arr.length; i++) {
+//        if (arr[i] > max) {
+//              max = arr[i]; 
+//          }
+// }
+// const maxIndex=arr.indexOf(max);
+// let arr2=arr.slice(1,maxIndex)
+// arr2.forEach(element=>sum+=element)        
+// console.log(sum);
+         
+
+
+//------------------------------------------------
+ //task4
+
+//  const arr=[123,7,53,99]
+// let max = arr[0], min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//          max = arr[i]; 
+//     }
+//     if (arr[i] < min) {
+//          min = arr[i]; 
+//     }
+// }
+//  let sum=0;
+// if (arr>max,min) {
+//     sum+=max+min
+//     sum=sum/2
+// }
+// console.log(sum);
+ 
+//------------------------------------------------
